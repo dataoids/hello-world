97e22646f5105b73a1129b7c23f10f4c3c1783f2
Naveen Bhardwaj <navee6252@gmail.com>
2023-01-14 12:27:34 +0530

    collectstatic

--- Python/Django/hello/settings.py
+++ Python/Django/hello/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u(2y%e3c#2^q@pp*i@9t&orxhq4pln@5iosai!%63qcyf7qog-'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DJANGO_DEBUG', default='False') == 'True')
 
 ALLOWED_HOSTS = []
 
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- Python/Django/init.ps1
+++ Python/Django/init.ps1
@@ -1,3 +1,10 @@
+# Environment variable
+$env:DJANGO_DEBUG = "True"
+$env:DJANGO_SECRET_KEY = "hello-world-dataoids"
+$env:DJANGO_SUPERUSER_USERNAME = "hello"
+$env:DJANGO_SUPERUSER_EMAIL = "hello@example.com"
+$env:DJANGO_SUPERUSER_PASSWORD = "world"
+
 # create the vitual env on the current directory
 python -m venv env
 
@@ -6,6 +13,16 @@ env\Scripts\activate
 
 # install django and other dependencies
 pip install -r requirements.txt
+
+# database table creation
+python manage.py makemigrations
+python manage.py migrate
+
+# collect static
+python manage.py collectstatic --noinput
+
+# create superuser
+python manage.py createsuperuser --noinput
 
 # run the development server
 python manage.py runserver
